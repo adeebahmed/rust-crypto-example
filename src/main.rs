b5209fe3bef67aba75506f66bb54de36a01d0fb9
AdeebAhmed <adeeb.ahmed@target.com>
2019-08-30 11:16:25 -0500

    Add example for SHA2-256

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ extern crate crypto;
 use self::crypto::digest::Digest;
 use self::crypto::sha3::Sha3;
 use crypto::sha2::Sha512;
+use crypto::sha2::Sha256;
 
 fn main() {
     sha3_256();
     sha2_512();
+    sha2_256()
 }
 
 fn sha3_256() {
@@ -19,8 +21,8 @@ fn sha3_256() {
     let hex = hasher.result_str();
     let hex_15 = &hex[0..15];
 
-    println!("SHA3 Hash is {:?}", hex);
-    println!("SHA3 Hash (15) {:?}", hex_15);
+    println!("SHA3-256 Hash is {:?}", hex);
+    println!("SHA3-256 Hash (15) {:?}", hex_15);
 }
 
 fn sha2_512() {
@@ -34,6 +36,21 @@ fn sha2_512() {
     let hex = hasher.result_str();
     let hex_15 = &hex[0..15];
 
-    println!("SHA2 Hash is {:?}", hex);
-    println!("SHA2 Hash (15) {:?}", hex_15);
+    println!("SHA2-512 Hash is {:?}", hex);
+    println!("SHA2-512 Hash (15) {:?}", hex_15);
+}
+
+fn sha2_256() {
+    // create a SHA2-256hasher
+    let mut hasher = Sha256::new();
+
+    // write input message
+    hasher.input_str("some_random_message");
+
+    // read hash digest
+    let hex = hasher.result_str();
+    let hex_15 = &hex[0..15];
+
+    println!("SHA2-256 Hash is {:?}", hex);
+    println!("SHA2-256 Hash (15) {:?}", hex_15);
 }
